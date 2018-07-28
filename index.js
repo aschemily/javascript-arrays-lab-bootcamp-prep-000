2d61edf1d720ecf3a8691c4adf2f0e6b25d47bc2
Emily Asch <e.asch@me.com>
2018-07-28 15:52:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your functions and code here
+function destructivelyAppendKitten (name){
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten (name){
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(){
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten (){
+  return kittens.shift();
+}
+
+function appendKitten (name){
+  return kittens.concat(name);
+}
+
+function prependKitten(name){
+  return kittens.slice(0);
+}
